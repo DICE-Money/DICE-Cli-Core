735cab4d92e8eb7eeb1ae90a7718fc12c3a7d1cc
Mihail Maldzhanski <pollarize@gmail.com>
2017-11-11 21:42:42 +0200

    Add test.js for testing is valid date in DICE Unit

---
+++ models/SwatchBeats/test.js
@@ -0,0 +1,47 @@
+/* 
+ * Copyright (c) 2017, Mihail Maldzhanski
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+const swatchTime = require('./SwatchTimer.js');
+const time = '36fa5d00';
+
+var timer = new swatchTime();
+
+//1. Create buffer 
+buffer = new Buffer(time, "hex");
+
+//2. Create Uin8Array + reverting it
+var u8Arr = new Uint8Array(buffer);
+//u8Arr = u8Arr.reverse();
+
+//3. Create Float array
+var u32Arr = new Uint32Array(u8Arr.buffer);
+
+//4. Get time 
+var realTime = timer.beatsToDate(u32Arr[0]);
+
+//5. Print time to Console
+console.log(u32Arr[0]);
+console.log(realTime);
