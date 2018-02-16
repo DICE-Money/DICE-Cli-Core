a55ed05dd77408a3377f6a734b0844d64850dc6a
Mihail Maldzhanski <pollarize@gmail.com>
2018-02-16 23:52:35 +0200

    Optimize address generation, validation and reduce invalid private keys.

--- models/AddressCalculator/Secp160k1.js
+++ models/AddressCalculator/Secp160k1.js
@@ -32,80 +32,145 @@ const modEc = require('elliptic').ec;
 var _Method = Secp160k1.prototype;
 
 //Local const
-const sizeOfKey = 20;
+const cSizeOfDA = 20;
+const cMinimuLengthOfPrivateKey = 7;
+const cLastThreeBitValue = 7;        // Represents 111 Binary
+const cPlusSign = "2";
+const cMinusSign = "3";
 
 function Secp160k1(keyPair) {
     this.keys = 0;
     this.ecSecp160k1 = new modEc('p160');
     this.cryptoSecp160k1 = modCrypto.createECDH("secp160k1");
-    
+
     //Set keys
     this.setKeys(keyPair);
 }
 
 _Method.getKeyPair = function () {
-    
-    //Convert public uncompressed to compressed
-    this.cryptoSecp160k1.setPrivateKey(this.keys.getPrivate("hex"),"hex");
-    var pub = this.cryptoSecp160k1.getPublicKey(null,"compressed");
 
-    return {private:this.keys.getPrivate(),public: pub.slice(0,sizeOfKey).toString('hex')};
+    //Convert public uncompressed to compressed
+    this.cryptoSecp160k1.setPrivateKey(this.keys.getPrivate("hex"), "hex");
+    var pub = this.cryptoSecp160k1.getPublicKey(null, "compressed");
+
+    return {private: this.keys.getPrivate(), public: this.compressPublicKey(pub)};
 };
 
 _Method.genarateKeys = function () {
-    var isReady = false;
+    var isReady = true;
     var pub, key;
-    
-    do{
+
+    do {
         key = this.ecSecp160k1.genKeyPair();
-        this.cryptoSecp160k1.setPrivateKey(key.getPrivate("hex"),"hex");
-        pub = this.cryptoSecp160k1.getPublicKey(null,"compressed");
+        this.cryptoSecp160k1.setPrivateKey(key.getPrivate("hex"), "hex");
+        pub = this.cryptoSecp160k1.getPublicKey(null, "compressed").toString("hex");
 
-        isReady = true;
-        for (var i = pub.length-1; i > (sizeOfKey-1);i-- )
-        {
-            if (pub[i] !== 0)
-            {
-                isReady =false;
-            }       
+        if (pub[cSizeOfDA * 2 + 1] <= cLastThreeBitValue) {
+            if (key.getPrivate().length === cMinimuLengthOfPrivateKey) {
+                isReady = false;
+            }
         }
+    } while (isReady)
 
-    }while(!isReady)
-   
     //Save 
     this.keys = key;
 };
 
-_Method.sign = function(hexString){
+_Method.sign = function (hexString) {
     //Sign data
     var signature = this.keys.sign(hexString.toString("hex"));
-    
+
     // Export DER encoded signature in Array 
     var derSign = signature.toDER();
-    
+
     return derSign;
 };
 
-_Method.verify = function(hexString, signature, pubKey){
-    var pubKeyReal = (pubKey.toString('hex') + "00");
+_Method.verify = function (hexString, signature, pubKey) {
+    var pubKeyReal = this.deCompressPublicKey(pubKey);
     var tempCryptoSecp160k1 = new modCrypto.createECDH("secp160k1");
-    var uncompressed =  tempCryptoSecp160k1.setPublicKey(pubKeyReal,"hex").getPublicKey("hex");
-    return this.ecSecp160k1.verify(hexString.toString("hex"), signature,uncompressed,"hex");
+    var uncompressed = tempCryptoSecp160k1.setPublicKey(pubKeyReal, "hex").getPublicKey("hex");
+    return this.ecSecp160k1.verify(hexString.toString("hex"), signature, uncompressed, "hex");
 };
 
-_Method.setKeys = function(keyPair){       
-    if (keyPair){
+_Method.setKeys = function (keyPair) {
+    if (keyPair) {
         this.keys = this.ecSecp160k1.keyFromPrivate(keyPair.private);
-        this.cryptoSecp160k1.setPrivateKey(keyPair.private.toString('hex'),"hex");
+        this.cryptoSecp160k1.setPrivateKey(keyPair.private.toString('hex'), "hex");
     }
 };
 
-_Method.getCryptoInstance = function(){
+_Method.getCryptoInstance = function () {
     return this.cryptoSecp160k1;
 };
 
-_Method.computeSecret = function(pubKey){
-    return this.cryptoSecp160k1.computeSecret((pubKey.toString('hex') + "00"), "hex");
+_Method.computeSecret = function (pubKey) {
+    var pubKeyReal = this.deCompressPublicKey(pubKey);
+    return this.cryptoSecp160k1.computeSecret(pubKeyReal, "hex");
 };
 
+_Method.compressPublicKey = function (pubKey) {
+    var pub = pubKey.toString("hex");
+    var address = "";
+    var secondFourBits = pub[1];
+    var lastFourBits = pub[cSizeOfDA * 2 + 1];
+
+    if (secondFourBits === cMinusSign)
+    {
+        address = dec2bin(lastFourBits);
+        address = address + "1";
+        while (address.length < 4)
+        {
+            address = "0" + address;
+        }
+    } else
+    {
+        address = dec2bin(lastFourBits);
+        address = address + "0";
+        while (address.length < 4)
+        {
+            address = "0" + address;
+        }
+    }
+    address = bin2dec(address).toString(16) + pub.slice(2, cSizeOfDA * 2 + 1);
+    return address;
+};
+
+_Method.deCompressPublicKey = function (pubKey) {
+    var pub = pubKey.toString("hex");
+    var address = "";
+    var firstFourBits = pub[0];
+    var sign = 0;
+    var lastFourBits = 0;
+
+    address = hex2bin(firstFourBits);
+    while (address.length < 4)
+    {
+        address = "0" + address;
+    }
+
+    if (address[address.length - 1] === "1") {
+        sign = cMinusSign;
+    } else {
+        sign = cPlusSign;
+    }
+
+    lastFourBits = bin2dec(address.slice(0, address.length - 1));
+
+    address = `0${sign}${pub.slice(1, pubKey.length)}${lastFourBits.toString(16)}`;
+    return address;
+};
+
+function dec2bin(dec) {
+    return (dec >>> 0).toString(2);
+}
+
+function bin2dec(binary) {
+    return parseInt(binary, 2);
+}
+
+function hex2bin(hex) {
+    var dec = parseInt(hex, 16);
+    return dec.toString(2);
+}
 module.exports = Secp160k1;
