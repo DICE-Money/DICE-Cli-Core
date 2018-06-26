4cc25fe814ff73d35b48a4880c54b7ce8e6ce6a6
Mihail Maldzhanski <pollarize@gmail.com>
2018-06-27 00:07:38 +0300

    Add needed folders and submodules
    DICE-Cli-Miner is a submodule of the current repository. Addes dependecies to modified npm packages and add instalation file.

---
+++ Install.bat
@@ -0,0 +1,14 @@
+@echo off
+
+echo Installing DICE-Money console application development kit
+echo.
+
+npm install
+
+npm -i -g nexe
+npm -i -g mocha
+npm -i -g mochawesome 
+
+echo.
+echo READY
+pause
