f36700c4ce5c47443ac46bba660a60989110f398
Mihail Maldzhanski <pollarize@gmail.com>
2018-06-28 23:35:01 +0300

    Add dependicies cloning in Install script

--- Install.bat
+++ Install.bat
@@ -3,6 +3,9 @@
 echo Installing DICE-Money console application development kit
 echo.
 
+git clone https://github.com/DICE-Money/DICE-Cli-Miner.git Apps/Miner
+git clone https://github.com/pollarize/elliptic.git 3rd-modified/elliptic
+
 npm install
 
 npm -i -g nexe
