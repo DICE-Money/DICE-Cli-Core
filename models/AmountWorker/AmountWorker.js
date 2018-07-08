e0b1718d30aff9f813ef8215c72a94214f9a2ceb
Mihail Maldzhanski <pollarize@gmail.com>
2018-07-09 01:49:44 +0300

    Add sorting of DICE units to create an amount

--- models/AmountWorker/AmountWorker.js
+++ models/AmountWorker/AmountWorker.js
@@ -31,12 +31,44 @@ function AmountWorker() {
     //Nothing
 }
 
-_Method.encode = function (data) {
-    return modBS58.encode(data);
+/* objUnitsDataList = {name, operator, owner, value}*/
+_Method.encodeAmount = function (arObjUnitsDataList, flTargetAmount) {
+    var intCurIndex = 0;
+    var flCurAmount = 0.0;
+    var intLasAddedIndex = 0;
+    var arUnits = [];
+    var flTargetAmountL = flTargetAmount / 1024;
+
+    // 1. Sort array of units - Bigger -> Smaller
+    var arObjUnitsDataList_Sorted = arObjUnitsDataList.sort(function (first, second) {
+        return first.value < second.value;
+    });
+
+    // 2,3 Execute 
+    do {
+        //Check if the sum overlaps and exchange last with the smallest one
+        if (flCurAmount > flTargetAmountL && 
+                arObjUnitsDataList_Sorted[intLasAddedIndex].value > arObjUnitsDataList_Sorted[intCurIndex].value) {
+            arUnits.pop();
+            flCurAmount -= arObjUnitsDataList_Sorted[intLasAddedIndex].value;
+        } else {
+            //Add only proper members
+            if (arObjUnitsDataList_Sorted[intCurIndex].value < flTargetAmountL) {
+                intLasAddedIndex = intCurIndex;
+                arUnits.push(arObjUnitsDataList_Sorted[intCurIndex].name);
+                flCurAmount += arObjUnitsDataList_Sorted[intCurIndex].value;
+            }
+        }
+        intCurIndex++;
+    } while (intCurIndex < arObjUnitsDataList_Sorted.length);
+
+    console.log(flCurAmount * 1024, flTargetAmountL);
+
+    return arUnits;
 };
 
-_Method.decode = function (data) {
-    return modBS58.decode(data);
+_Method.decodeAmount = function (objUnitsDataList) {
+
 };
 
 
